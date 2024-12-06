e059603a3afe845598686d0fc1c28ff660891f32
OSavosina <kcuxa4@yandex.ru>
2024-12-07 00:00:15 +0300

    Add files via upload
    First files

---
+++ constants.py
@@ -0,0 +1,15 @@
+class Constants:
+    MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site'
+    CONSTRUCTOR_PAGE_URL = 'https://stellarburgers.nomoreparties.site/'
+    SIGN_IN_URL = 'https://stellarburgers.nomoreparties.site/login'
+    SIGN_UP_URL = 'https://stellarburgers.nomoreparties.site/register'
+    FORGOT_PASSWORD_URL = 'https://stellarburgers.nomoreparties.site/forgot-password'
+    USER_PROFILE_PAGE_URL = 'https://stellarburgers.nomoreparties.site/account/profile'
+
+class UserLogin:
+    USER_NAME = 'o.savosina'
+    USER_EMAIL = 'o.savosina1616@yandex.ru'
+    USER_PASSWORD = '123456'
+
+class Messages:
+    SIGN_UP_PASSWORD_ERROR = 'Некорректный пароль'
