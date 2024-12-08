bce2b24366e7c94242b14fd9789de844d31f071e
OSavosina <kcuxa4@yandex.ru>
2024-12-08 23:29:32 +0300

    Update test_sign_in_success.py

--- tests/test_sign_in_success.py
+++ tests/test_sign_in_success.py
@@ -1,16 +1,11 @@
-from selenium import webdriver
+from conftest import driver
 
-from constants import Constants, UserLogin
+from constants import UserLogin
 from locators import Locators
 
 class TestSignIn:
-    def test_sign_in_success(self):
-        driver = webdriver.Chrome()
-        driver.get(Constants.SIGN_IN_URL)
-
+    def test_sign_in_success(self, driver):
         driver.find_element(*Locators.SIGN_IN_FIELD_EMAIL).send_keys(UserLogin.USER_EMAIL)
         driver.find_element(*Locators.SIGN_IN_FIELD_PASSWORD).send_keys(UserLogin.USER_PASSWORD)
 
         driver.find_element(*Locators.SIGN_IN_BUTTON).click()
-
-        driver.quit()
