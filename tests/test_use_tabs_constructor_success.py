e53522ece4c032ffeba331e64f463b0c12cc5e1e
OSavosina <kcuxa4@yandex.ru>
2024-12-08 23:30:28 +0300

    Update test_use_tabs_constructor_success.py

--- tests/test_use_tabs_constructor_success.py
+++ tests/test_use_tabs_constructor_success.py
@@ -1,14 +1,11 @@
-from selenium import webdriver
+from conftest import driver
 
 from constants import Constants
 from locators import Locators
 
 class TestUseTabsConstructorSuccess:
-    def test_use_tabs_souses_constructor_success(self):
-        driver = webdriver.Chrome()
-        driver.get(Constants.MAIN_PAGE_URL)
+    def test_use_tabs_souses_constructor_success(self, driver):
         tabs_list = []
-
 
         driver.find_element(*Locators.TAB_SOUSES).click()
         tabs_list.append(driver.find_element(*Locators.SOUSE_ELEMENT).text)
@@ -21,4 +18,3 @@ class TestUseTabsConstructorSuccess:
 
         assert '0\n90\nСоус Spicy-X' and '0\n1337\nМясо бессмертных моллюсков Protostomia' and '0\n988\nФлюоресцентная булка R2-D3' in tabs_list
 
-        driver.quit()
