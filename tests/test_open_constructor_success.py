9e11a77944f2742061f6e872c8c159f21bc2aa85
OSavosina <kcuxa4@yandex.ru>
2024-12-08 23:28:06 +0300

    Update test_open_constructor_success.py

--- tests/test_open_constructor_success.py
+++ tests/test_open_constructor_success.py
@@ -1,26 +1,17 @@
-from selenium import webdriver
+from conftest import driver
 
 from constants import Constants
 from locators import Locators
 
 
 class TestOpenConstructorSuccess:
-    def test_open_constructor_success(self):
-        driver = webdriver.Chrome()
-        driver.get(Constants.MAIN_PAGE_URL)
-
+    def test_open_constructor_success(self, driver):
         driver.find_element(*Locators.CONSTRUCTOR_LINK).click()
 
         assert driver.current_url == Constants.CONSTRUCTOR_PAGE_URL
 
-        driver.quit()
 
-    def test_open_constructor_by_logo_success(self):
-        driver = webdriver.Chrome()
-        driver.get(Constants.MAIN_PAGE_URL)
-
+    def test_open_constructor_by_logo_success(self, driver):
         driver.find_element(*Locators.STELLAR_BURGERS_LOGO).click()
 
         assert driver.current_url == Constants.CONSTRUCTOR_PAGE_URL
-
-        driver.quit()
