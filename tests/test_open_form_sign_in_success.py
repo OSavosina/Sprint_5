5fac539b6075d3929c3b8fd902959bf9b46f0c97
OSavosina <kcuxa4@yandex.ru>
2024-12-08 23:28:28 +0300

    Update test_open_form_sign_in_success.py

--- tests/test_open_form_sign_in_success.py
+++ tests/test_open_form_sign_in_success.py
@@ -1,41 +1,25 @@
-from selenium import webdriver
+from conftest import driver
 
 from constants import Constants
 from locators import Locators
 
 class TestOpenFormSignInSuccess:
-    def test_open_form_sign_in_by_button_enter_success(self):
-        driver = webdriver.Chrome()
-        driver.get(Constants.MAIN_PAGE_URL)
+    def test_open_form_sign_in_by_button_enter_success(self, driver):
 
         driver.find_element(*Locators.ENTER_TO_LOGIN_BUTTON).click()
         assert driver.current_url == Constants.SIGN_IN_URL
 
-        driver.quit()
 
-    def test_open_form_sign_in_by_button_lk_success(self):
-        driver = webdriver.Chrome()
-        driver.get(Constants.MAIN_PAGE_URL)
-
+    def test_open_form_sign_in_by_button_lk_success(self, driver):
         driver.find_element(*Locators.OPEN_USER_PROFILE_BUTTON).click()
         assert driver.current_url == Constants.SIGN_IN_URL
 
-        driver.quit()
 
-    def test_open_form_sign_in_from_sign_up_page_success(self):
-        driver = webdriver.Chrome()
-        driver.get(Constants.SIGN_UP_URL)
-
+    def test_open_form_sign_in_from_sign_up_page_success(self, driver):
         driver.find_element(*Locators.OPEN_USER_PROFILE_LINK).click()
         assert driver.current_url == Constants.SIGN_IN_URL
 
-        driver.quit()
 
-    def test_open_form_sign_in_from_forgot_password_page_success(self):
-        driver = webdriver.Chrome()
-        driver.get(Constants.FORGOT_PASSWORD_URL)
-
+    def test_open_form_sign_in_from_forgot_password_page_success(self, driver):
         driver.find_element(*Locators.OPEN_USER_PROFILE_LINK).click()
         assert driver.current_url == Constants.SIGN_IN_URL
-
-        driver.quit()
