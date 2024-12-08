b0353166d9c5b93aecd1e15692cf38035b11281f
OSavosina <kcuxa4@yandex.ru>
2024-12-08 23:29:13 +0300

    Update test_open_profile_page_success.py

--- tests/test_open_profile_page_success.py
+++ tests/test_open_profile_page_success.py
@@ -1,14 +1,11 @@
-from selenium import webdriver
+from conftest import driver
 
 from constants import Constants, UserLogin
 from locators import Locators
 
 
 class TestOpenForgotPasswordPageSuccess:
-    def test_open_forgot_password_page_success(self):
-        driver = webdriver.Chrome()
-        driver.get(Constants.SIGN_UP_URL)
-
+    def test_open_forgot_password_page_success(self, driver):
         driver.find_element(*Locators.SIGN_UP_FIELD_NAME).send_keys(UserLogin.USER_NAME)
         driver.find_element(*Locators.SIGN_UP_FIELD_EMAIL).send_keys(UserLogin.USER_EMAIL)
         driver.find_element(*Locators.SIGN_UP_FIELD_PASSWORD).send_keys(UserLogin.USER_PASSWORD)
@@ -16,5 +13,3 @@ class TestOpenForgotPasswordPageSuccess:
         driver.find_element(*Locators.OPEN_USER_PROFILE_BUTTON).click()
         driver.get(Constants.FORGOT_PASSWORD_URL)
         assert driver.current_url == Constants.FORGOT_PASSWORD_URL
-
-        driver.quit()
