ca98d6fca8aa54896bf916388526a96ca8556b3e
OSavosina <kcuxa4@yandex.ru>
2024-12-08 23:30:06 +0300

    Update test_sign_up_success.py

--- tests/test_sign_up_success.py
+++ tests/test_sign_up_success.py
@@ -1,17 +1,12 @@
-from selenium import webdriver
+from conftest import driver
 
-from constants import Constants, UserLogin
+from constants import UserLogin
 from locators import Locators
 
 class TestSignUp:
-    def test_sign_up_password_success(self):
-        driver = webdriver.Chrome()
-        driver.get(Constants.SIGN_UP_URL)
-
+    def test_sign_up_password_success(self, driver):
         driver.find_element(*Locators.SIGN_UP_FIELD_NAME).send_keys(UserLogin.USER_NAME)
         driver.find_element(*Locators.SIGN_UP_FIELD_EMAIL).send_keys(UserLogin.USER_EMAIL)
         driver.find_element(*Locators.SIGN_UP_FIELD_PASSWORD).send_keys(UserLogin.USER_PASSWORD)
 
         driver.find_element(*Locators.SIGN_UP_BUTTON).click()
-
-        driver.quit()
